8b9dc09a3c859cac86f7e92e3061559b70288852
Dwi Marfia <34159879+dwimarfia@users.noreply.github.com>
2018-03-07 01:16:34 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -24,7 +24,8 @@ var xxx=null;
 if(x=="1"){xxx="go.html"}
 if(x=="2"){xxx="go1.html"}
 if(x=="3"){xxx="go2.html"}
-if(x=="4"){xxx="go3.html"}a_to_ve[i].href="https://www.markupit.com/2018/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="go3.html"}
+if(x=="5"){xxx="go4.html"}a_to_ve[i].href="https://www.markupit.com/2018/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
